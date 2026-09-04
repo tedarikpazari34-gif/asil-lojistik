2d3fe646ade488bd1ccb533b8b6fb8fdbc9c4791
NAZ ÇETİN <nazcetin@192.168.1.100>
2026-09-04 20:05:17 +0300

    Add Eskisehir location

--- src/App.tsx
+++ src/App.tsx
@@ -147,6 +147,7 @@ function App() {
               <div className="contactDetails">
                 <a href="tel:+905323833905">0532 383 39 05</a>
                 <a href="mailto:nesetilica@outlook.com">nesetilica@outlook.com</a>
+                <span>Eskişehir</span>
               </div>
             </div>
 
